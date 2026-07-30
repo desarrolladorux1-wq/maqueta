f0fd3729bbb2607b8c56ed2f64f1615a342100ef
desarrolladorux1-wq <desarrolladorux_1@paulet.space>
2026-07-30 13:51:23 -0500

    Delete work directory

--- work/check-fotovoltaico-inline.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const vm = require('vm');
-const file = 'modulos/fotovoltaico/fotovoltaico.html';
-const html = fs.readFileSync(file, 'utf8');
-const scripts = [...html.matchAll(/<script(?:\s[^>]*)?>([\s\S]*?)<\/script>/gi)].map(match => match[1]).filter(Boolean);
-scripts.forEach((script, index) => new vm.Script(script, { filename: `${file}#script-${index + 1}` }));
-console.log(`OK ${scripts.length} inline scripts`);
